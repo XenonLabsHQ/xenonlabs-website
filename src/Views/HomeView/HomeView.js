4f362993ee2df7c099ece1c36f5ecab3f1e6164a
Sundev79 <sundev79@nutdevs.org>
2022-01-16 18:08:44 +0100

    Views: Added HomeView

---
+++ src/Views/HomeView/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainHeader from '../../Modules/MainHeader/MainHeader'
+
+const HomeView = () => {
+    return (
+        <div className="activity activity-homeview">
+            <MainHeader />
+        </div>
+    )
+}
+
+export default HomeView
