484a1397b3193b96ed9ff98ef8b036b26ab46447
Sundev79 <sundev79@nutdevs.org>
2022-01-16 18:23:03 +0100

    Modules: MainHeader: Added mh-menuitems

--- src/Modules/MainHeader/MainHeader.js
+++ src/Modules/MainHeader/MainHeader.js
@@ -5,6 +5,12 @@ const MainHeader = () => {
     return (
         <header className="main-header">
             <div className="mh-title">XenonLabs</div>
+            
+            <div className="mh-menuitems">
+                <div className="mhmi-item">Acceuil</div>
+                <div className="mhmi-item">A Propos de Nous</div>
+                <div className="mhmi-item">Contactez-nous</div>
+            </div>
         </header>
     )
 }
