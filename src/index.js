5adb1f9c8eddb456b2d2f4a1e94edce4739e433f
Sundev79 <sundev79@nutdevs.org>
2022-01-16 18:10:40 +0100

    PROJ: Added HomeView

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import "./Common/main.scss";
+import HomeView from './Views/HomeView/HomeView';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeView />
   </React.StrictMode>,
   document.getElementById('root')
 );
