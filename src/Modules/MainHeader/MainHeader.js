d31063676a2f1a3265364144986b70a7c48b342a
Sundev79 <sundev79@nutdevs.org>
2022-01-16 18:09:05 +0100

    Modules: Added MainHeader

---
+++ src/Modules/MainHeader/MainHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./MainHeader.scss";
+
+const MainHeader = () => {
+    return (
+        <header className="main-header">
+            <div className="mh-title">XenonLabs</div>
+        </header>
+    )
+}
+
+export default MainHeader
