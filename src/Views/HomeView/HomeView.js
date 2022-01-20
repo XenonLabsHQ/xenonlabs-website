ce675839969493d85fbe4d1bda7b3b620f526612
Sundev79 <sundev79@nutdevs.org>
2022-01-20 14:11:13 +0100

    Modules: HomeView: Added a first section !

--- src/Views/HomeView/HomeView.js
+++ src/Views/HomeView/HomeView.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import MainHeader from '../../Modules/MainHeader/MainHeader'
+import React from "react";
+import MainHeader from "../../Modules/MainHeader/MainHeader";
+import "./HomeView.scss";
 
 const HomeView = () => {
-    return (
-        <div className="activity activity-homeview">
-            <MainHeader />
+  return (
+    <div className="activity activity-homeview">
+      <MainHeader />
 
-            <div className="section section-home" id="home">
-                Hello World !
-            </div>
+      <div className="sections">
+        <div className="section section-home" id="home">
+          Hello World !
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default HomeView
+export default HomeView;
