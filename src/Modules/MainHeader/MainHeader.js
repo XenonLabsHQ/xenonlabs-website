4a24bb6f44dad0ca57721094daac506f51cdd1eb
Sundev79 <sundev79@nutdevs.org>
2022-01-21 14:16:00 +0100

    Modules: MainHeader: Updated logotext - make more evident

--- src/Modules/MainHeader/MainHeader.js
+++ src/Modules/MainHeader/MainHeader.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from "react";
 import "./MainHeader.scss";
 
 const MainHeader = () => {
-    return (
-        <header className="main-header">
-            <div className="mh-title">XenonLabs</div>
-            
-            <div className="mh-menuitems">
-                <div className="mhmi-item">Acceuil</div>
-                <div className="mhmi-item">Panel Serveurs</div>
-                <div className="mhmi-item">A Propos de Nous</div>
-                <div className="mhmi-item">Contactez-nous</div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="main-header">
+      <div className="mh-title">
+        <span className="mh-title-label-xenon">Xenon</span>Labs
+      </div>
 
-export default MainHeader
+      <div className="mh-menuitems">
+        <div className="mhmi-item">Acceuil</div>
+        <div className="mhmi-item">Panel Serveurs</div>
+        <div className="mhmi-item">A Propos de Nous</div>
+        <div className="mhmi-item">Contactez-nous</div>
+      </div>
+    </header>
+  );
+};
+
+export default MainHeader;
