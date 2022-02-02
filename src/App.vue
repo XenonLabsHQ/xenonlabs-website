d2c813193fd79c14c73b3db44b91bf8a47220b37
sigmanificient <edhyjox@gmail.com>
2022-02-02 14:55:41 +0100

    :lipstick: reset and base

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,21 @@
 		name: 'App'
 	}
 </script>
+
+<style>
+:root {
+  --bg-default: #294523;
+  --bg-evidence: #74e262;
+  --bg-darker: #5bb04c;
+  --bg-trans-darker: #57575750;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: 0;
+  min-height: 100vh;
+}
+</style>
