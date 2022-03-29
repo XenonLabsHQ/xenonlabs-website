cae0d83cd35080d2c6594ec64bec1bba4c0d5e6b
sigmanificient <sigmanificient@gmail.com>
2022-03-30 00:10:28 +0200

    :sparkles: Adding wip page

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,5 +3,10 @@ export default [
         path: "/",
         name: "home",
         component: () => import("@/views/HomeView.vue"),
-    }
+    },
+    {
+        path: "/wip",
+        name: "wip",
+        component: () => import("@/views/wip/HomeView.vue"),
+    },
 ];
---
+++ src/views/wip/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+	<p>Hello world!</p>
+</template>
+
+<script></script>
+
+<style lang="scss"></style>
