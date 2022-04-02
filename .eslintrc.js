167a56ee4505c441dcd26cc5e451368977db8c4b
Powerm1nt <sundev79@nutdevs.org>
2022-04-02 14:16:45 +0200

    PROJ: Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+  extends: [
+    "plugin:vue/essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+  ],
 };
