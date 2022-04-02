fba1cfa3d07d0a2b7702aa8061dad05de42bde56
sigmanificient <sigmanificient@gmail.com>
2022-04-02 22:45:39 +0200

    :recycle: Improving css

--- src/components/wip/TheHeaderMenu.vue
+++ src/components/wip/TheHeaderMenu.vue
@@ -1,21 +1,21 @@
 <template>
-  <nav class="menu-cmpnt">
-    <ul class="menu-items" :class="{ 'menu-open': isOpen }">
-      <li class="menu-item">Accueil</li>
-      <li class="menu-item">Pourquoi nous</li>
-      <li class="menu-item">Notre équipe</li>
-      <li class="menu-item">Contact</li>
+  <nav class="nav">
+    <ul class="nav__list" :class="{ nav__list__open: isOpen }">
+      <li class="nav__item">Accueil</li>
+      <li class="nav__item">Pourquoi nous</li>
+      <li class="nav__item">Notre équipe</li>
+      <li class="nav__item">Contact</li>
     </ul>
 
-    <div class="menu-btn" @click="isOpen = !isOpen">
-      <div class="hamburger">
-        <div class="ham-line" />
-        <div class="ham-line" />
-        <div class="ham-line" />
+    <div class="nav__btn" @click="isOpen = !isOpen">
+      <div class="nav__hamburger">
+        <div class="nav__hamburger__line" />
+        <div class="nav__hamburger__line" />
+        <div class="nav__hamburger__line" />
       </div>
     </div>
 
-    <div class="menu-bar"></div>
+    <div class="nav__bar"></div>
   </nav>
 </template>
 
@@ -46,8 +46,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.menu-cmpnt {
-  .menu-btn {
+.nav {
+  &__list {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    inset: 0;
+    padding: 0;
+    opacity: 0;
+
+    background-color: rgb(#000, 75%);
+    transition: all 0.1s ease-in-out;
+    margin-block: 0;
+
+    &__open {
+      opacity: 1;
+    }
+  }
+
+  &__item {
+    list-style-type: none;
+    text-align: center;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 32pt;
+    font-weight: 600;
+    color: white;
+    padding: 15px 20px;
+
+    @media screen and (max-width: 550px) {
+      padding: 15px 0;
+      font-size: 8vw;
+    }
+
+    &:hover {
+      color: var(--accent-2nd);
+    }
+  }
+
+  &__btn {
     user-select: none;
     position: relative;
     z-index: 2;
@@ -56,77 +96,20 @@ export default {
     @media screen and (max-width: 1080px) {
       display: initial;
     }
+  }
 
-    .hamburger {
-      display: flex;
-      flex-direction: column;
-      gap: 5px;
-      width: 30px;
+  &__hamburger {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    width: 30px;
 
-      .ham-line {
-        z-index: 2;
-        height: 3px;
-        background-color: white;
-        border-radius: 300px;
-      }
+    &__line {
+      z-index: 2;
+      height: 3px;
+      background-color: white;
+      border-radius: 300px;
     }
   }
-
-  .menu-overlay {
-    z-index: -1;
-    background: rgba(0, 0, 0, 0.5);
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    min-width: 100vw;
-    min-height: 100vh;
-    transition: opacity 0.2s ease-in-out;
-    display: none;
-
-    @media screen and (max-width: 1080px) {
-      display: initial;
-    }
-  }
-}
-
-.menu-items {
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  inset: 0;
-  padding: 0;
-  opacity: 0;
-
-  background-color: rgb(#000, 75%);
-  transition: all 0.1s ease-in-out;
-  margin-block: 0;
-}
-
-.menu-item {
-  list-style-type: none;
-  text-align: center;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  font-size: 32pt;
-  font-weight: 600;
-  color: white;
-  padding: 15px 20px;
-
-  @media screen and (max-width: 550px) {
-    padding: 15px 0;
-    font-size: 8vw;
-  }
-
-  &:hover {
-    color: var(--accent-2nd);
-  }
-}
-
-.menu-open {
-  opacity: 1;
 }
 </style>
