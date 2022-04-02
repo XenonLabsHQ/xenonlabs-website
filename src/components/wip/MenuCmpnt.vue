c7c28ffe99eee4ef314bf5dc57c2a204fb0f7fdd
Powerm1nt <sundev79@nutdevs.org>
2022-04-02 18:39:15 +0200

    MenuCmpnt: Transitions, and mobile only display now works !

--- src/components/wip/MenuCmpnt.vue
+++ src/components/wip/MenuCmpnt.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="cmpnt menu-cmpnt">
-    <div class="menu-btn" @click="ontoggle">
+    <div class="menu-btn" @click="isOpen = !isOpen">
       <img src="@/assets/icons/hamburger.svg" alt="" />
     </div>
 
-    <div class="menu-overlay">
+    <div class="menu-overlay" :class="{ 'menu-overlay-open': isOpen }">
       <div class="menu-ov-container">
         <div class="menu-items">
           <div class="menu-item">Accueil</div>
@@ -21,9 +21,24 @@
 <script>
 export default {
   name: "MenuCmpnt",
+
+  created() {
+    window.addEventListener("resize", this.onRezize);
+  },
+
+  destroyed() {
+    window.removeEventListener("resize", this.onRezize);
+  },
+
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+
   methods: {
-    ontoggle() {
-      console.log("Toggled !");
+    onRezize() {
+      this.isOpen = false;
     },
   },
 };
@@ -31,13 +46,37 @@ export default {
 
 <style lang="scss" scoped>
 .menu-cmpnt {
+  .menu-btn {
+    user-select: none;
+    position: relative;
+    z-index: 2;
+    display: none;
+
+    @media screen and (max-width: 1080px) {
+      display: initial;
+    }
+  }
+
   .menu-overlay {
+    z-index: -1;
     background: rgba(0, 0, 0, 0.5);
     position: absolute;
     top: 0;
     left: 0;
+    opacity: 0;
     min-width: 100vw;
     min-height: 100vh;
+    transition: opacity 0.2s ease-in-out;
+    display: none;
+
+    @media screen and (max-width: 1080px) {
+      display: initial;
+    }
+
+    &-open {
+      z-index: 1;
+      opacity: 1;
+    }
 
     .menu-ov-container {
       min-height: 100vh;
@@ -47,12 +86,18 @@ export default {
 
       .menu-items {
         padding: 100px;
+        user-select: none;
 
         .menu-item {
           text-align: center;
           font-size: 30pt;
           color: white;
-          padding: 15px 10px;
+          padding: 15px 20px;
+          transition: color 0.1s ease-in-out;
+
+          &:hover {
+            color: var(--accent-2nd);
+          }
         }
       }
     }
