a1786c28de74200ff1116dc111cbf059b06a258e
sigmanificient <sigmanificient@gmail.com>
2022-04-03 20:19:32 +0200

    :white_check_mark: testing previous card

--- src/views/wip/TestView.vue
+++ src/views/wip/TestView.vue
@@ -1,10 +1,15 @@
 <template>
-	<h1>Test Zone</h1>
+	<main>
+		<h1>Test Zone</h1>
+		<card-info-dev name="Sigma" img="Sigma.png" />
+	</main>
 </template>
 
 <script>
+import CardInfoDev from "@/components/wip/CardInfoMember.vue";
 export default {
 	name: "TestView",
+	components: { CardInfoDev },
 };
 </script>
 
@@ -13,5 +18,8 @@ body {
 	color: white;
 	background-color: #0a0c0a;
 	margin: 1em;
+	font-family: "Outfit", sans-serif;
+	font-size: clamp(12px, 1vw, 18px);
+	overflow-x: hidden;
 }
 </style>
