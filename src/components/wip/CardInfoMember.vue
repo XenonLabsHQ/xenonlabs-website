e567785abda889180192593c011600d4e3c17e41
Powerm1nt <sundev79@nutdevs.org>
2022-04-05 20:28:44 +0200

    :fix: Standard name

--- src/components/wip/CardInfoMember.vue
+++ src/components/wip/CardInfoMember.vue
@@ -4,17 +4,17 @@
 			<img class="info--member__avatar" :src="'src/assets/members/' + img" :alt="name + ' profile picture'" />
 			<p class="info--member__name">{{ name }}</p>
 		</div>
-		<button-green>En savoir plus</button-green>
+		<base-button class="btn__base__primary">En savoir plus</base-button>
 	</div>
 </template>
 
 <script>
-import ButtonGreen from "@/components/wip/ButtonGreen.vue";
+import BaseButton from "@/components/wip/BaseButton.vue";
 
 export default {
-	name: "CardInfoDev",
+	name: "CardInfoMember",
 	components: {
-		ButtonGreen,
+		BaseButton,
 	},
 	props: {
 		name: {
