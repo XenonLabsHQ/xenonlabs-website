6d866a6bd0048d0c8a30a54664ae4098489c3f7a
Powerm1nt <sundev79@nutdevs.org>
2022-04-05 20:46:26 +0200

    :art: Change styling code to SCSS CSS style.

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -23,87 +23,101 @@ export default {
 };
 </script>
 
-<style lang="stylus">
-@keyframes anim-mobile-logo
-  from
-    opacity: 0
+<style lang="scss">
+@keyframes anim-mobile-logo {
+	from {
+		opacity: 0;
+	}
 
-  to
-    opacity: 1
+	to {
+		opacity: 1;
+	}
+}
 
+.main-header {
+	height: 80px;
+	background-color: var(--bg-darker);
+	position: sticky;
+	top: 0;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	gap: 2em;
+	padding: 0 5%;
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.349);
+	border-bottom: 1px solid #505050b3;
 
-.main-header
-  height: 80px
-  background: var(--bg-darker)
-  position: sticky
-  top: 0
-  display: flex
-  align-items: center
-  justify-content: space-between
-  gap: 2em;
-  padding: 0 5%;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.349)
-  border-bottom: 1px solid #505050b3
+	@supports (backdrop-filter: blur()) {
+		backdrop-filter: blur(20px);
+		background-color: var(--bg-trans-darker);
+	}
 
-  @supports (backdrop-filter: blur())
-    backdrop-filter: blur(20px)
-    background: var(--bg-trans-darker)
+	@media screen and (max-width: 1080px) {
+		display: none;
+	}
 
-  @media screen and (max-width: 1080px)
-    display: none
+	.mh-title {
+		display: flex;
+		font-size: 30pt;
+		color: white;
+		font-weight: 200;
 
-  .mh-title
-    display: flex
-    font-size: 30pt
-    color: white
-    font-weight: 200
+		.mh-title-label-xenon {
+			font-weight: 600;
+		}
 
-    .mh-title-label-xenon
-      font-weight: 600
+		.mhtilx-dot {
+			font-weight: 500;
+		}
+	}
 
-    .mhtilx-dot
-      font-weight: 500
+	.mh-menuitems {
+		display: flex;
+		align-items: center;
+		gap: 20px;
+		user-select: none;
 
-  .mh-menuitems
-    display: flex
-    align-items: center
-    gap: 20px
-    user-select: none
+		.mhmi-item {
+			color: white;
+			font-size: 18px;
+			height: 22px;
+			text-decoration: none;
+			padding: 5px;
 
-    .mhmi-item
-      color: white
-      font-size: 18px
-      height: 22px
-      text-decoration: none
-      padding: 5px
+			&:after {
+				content: "";
+				display: block;
+				width: 100%;
+				height: 3px;
+				position: relative;
+				top: 5px;
+				opacity: 0;
+				background-color: white;
+				transition: 0.1s opacity ease-in-out, 0.1s top ease-in-out;
+				pointer-events: none;
+				user-select: none;
+				border-radius: 300px;
+				cursor: pointer;
+			}
 
-      &:after {
-        content: ""
-        display: block
-        width: 100%
-        height: 3px
-        position: relative
-        top: 5px
-        opacity: 0
-        background: white
-        transition: 0.1s opacity ease-in-out, 0.1s top ease-in-out
-        pointer-events: none
-        user-select: none
-        border-radius: 300px
-        cursor: pointer
-      }
+			&:hover {
+				&::after {
+					opacity: 1;
+					top: 0;
+				}
+			}
+		}
+	}
 
-      &:hover
-        &::after
-          opacity: 1
-          top: 0
+	.separator {
+		background-color: #e4e1e1;
+		display: inline-block;
+		width: 3px;
+		border-radius: 300px;
 
-.separator
-  background-color: #e4e1e1
-  display: inline-block
-  width: 3px
-  border-radius: 300px
-
-  &-vertical
-    height: 25px
+		&-vertical {
+			height: 25px;
+		}
+	}
+}
 </style>
