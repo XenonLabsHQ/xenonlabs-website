ac009653e4b475110ddd25bf43cf2c832ea9fe81
sigmanificient <sigmanificient@gmail.com>
2022-04-07 21:01:19 +0200

    :bug: ESLint wasn't using vue3 specific essential

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     root: true,
     extends: [
         "plugin:vue/base",
-        "plugin:vue/essential",
+        "plugin:vue/vue3-essential",
         "plugin:vue/vue3-strongly-recommended",
         "plugin:vue/vue3-recommended",
         "plugin:prettier/recommended",
