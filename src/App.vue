ce65dc54228f74b711d69cfb713ae1ae93d647b8
powerm1nt <nukaworks@nuka.works>
2022-05-18 21:24:38 +0200

    :lipstick: Updated Outfit font import

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;600&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap");
 
 :root {
 	--bg-default: #294523;
