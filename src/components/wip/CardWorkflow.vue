17b5c00fb2b3f82bc4583ee14c87870c76ca3e25
sigmanificient <edhyjox@gmail.com>
2022-05-20 10:06:53 +0200

    :sparkles: Adding Workflow cards

---
+++ src/components/wip/CardWorkflow.vue
@@ -0,0 +1,49 @@
+<template>
+	<card class="workflow">
+		<img :src="'src/assets/icons/' + img" :alt="alt" />
+		<p>{{ description }}</p>
+	</card>
+</template>
+
+<script>
+export default {
+	name: "CardWorkflow",
+	props: {
+		description: {
+			type: String,
+			required: true,
+		},
+		img: {
+			type: String,
+			required: true,
+		},
+		alt: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+.workflow {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: var(--green-400);
+	color: var(--text-color-2nd);
+	border-radius: var(--radius-big);
+	padding: 1em;
+	gap: 1em;
+
+	& > img {
+		width: 5em;
+		height: auto;
+	}
+
+	& > p {
+		margin: 0;
+		width: 25ch;
+	}
+}
+</style>
--- src/views/wip/HomeView.vue
+++ src/views/wip/HomeView.vue
@@ -8,7 +8,24 @@
 		<TheQualityCards />
 	</section>
 	<section id="more">
-		<p>Hello!</p>
+		<h1>Choisissez notre système innovent</h1>
+		<div class="workflows">
+			<CardWorkflow
+				description="Nous n'avons pas de grille tarifaire, nous choisissions avec vous un prix correct et cohérent."
+				img="cash.svg"
+				alt="Payment"
+			/>
+			<CardWorkflow
+				description="Nos développeurs et graphistes sont indépendant et touchent la quasi-totalité de l'argent gagné."
+				img="brush.svg"
+				alt="Gain"
+			/>
+			<CardWorkflow
+				description="Vous serez mis directement en relation avec l'équipe. Ni relais ni intermédiaire."
+				img="people.svg"
+				alt="Contact"
+			/>
+		</div>
 	</section>
 </template>
 
@@ -17,9 +34,10 @@ import TheHeader from "@/components/wip/TheHeader.vue";
 import TheHero from "@/components/wip/TheHero.vue";
 import TheServices from "@/components/wip/TheServices.vue";
 import TheQualityCards from "@/components/wip/TheQualityCards.vue";
+import CardWorkflow from "@/components/wip/CardWorkflow.vue";
 
 export default {
-	components: { TheQualityCards, TheServices, TheHero, TheHeader },
+	components: { CardWorkflow, TheQualityCards, TheServices, TheHero, TheHeader },
 };
 </script>
 
@@ -73,7 +91,6 @@ body {
 	justify-content: space-between;
 	flex-direction: column;
 	gap: 4em;
-	border-bottom: 0.5em solid var(--bg);
 
 	&__focus {
 		display: flex;
@@ -86,7 +103,24 @@ body {
 }
 
 #more {
-	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	box-sizing: border-box;
+	border-top: 1em solid var(--bg);
+	border-bottom: 1em solid var(--bg);
 	background-color: var(--black-400);
+	gap: 4em;
+	height: 50vh;
+}
+
+.workflows {
+	display: flex;
+	flex-flow: row wrap;
+	align-items: center;
+	justify-content: center;
+	gap: 2em;
+	max-width: 60%;
 }
 </style>
