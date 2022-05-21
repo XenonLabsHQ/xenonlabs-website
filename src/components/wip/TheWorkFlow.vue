180436572a590d08cbec6c4839781f58ca563346
powerm1nt <nukaworks@nuka.works>
2022-05-21 17:19:02 +0200

    :recycle: Moved CardWorkflow

--- src/components/wip/TheWorkFlow.vue
+++ src/components/wip/TheWorkFlow.vue
@@ -2,33 +2,27 @@
 	<section id="more">
 		<h1>Choisissez notre système innovant</h1>
 		<div class="workflows">
-			<CardWorkflow
-				description="Nous n'avons pas de grille tarifaire, nous choisissions avec vous un prix correct et cohérent."
-				img="cash.svg"
-				alt="Paiement"
-			/>
-			<CardWorkflow
-				description="Nos développeurs et graphistes sont indépendant et touchent la quasi-totalité de l'argent gagné."
-				img="brush.svg"
-				alt="Gain"
-			/>
-			<CardWorkflow
-				description="Vous serez mis directement en relation avec l'équipe. Ni relais ni intermédiaire."
-				img="people.svg"
-				alt="Contact"
-			/>
+			<div class="workflow--cards">
+				<img src="@/assets/icons/cash.svg" alt="Paiement" />
+				<p>Nous n'avons pas de grille tarifaire, nous choisissions avec vous un prix correct et cohérent.</p>
+			</div>
+
+			<div class="workflow--cards">
+				<img src="@/assets/icons/brush.svg" alt="Gain" />
+				<p>Nos développeurs et graphistes sont indépendant et touchent la quasi-totalité de l'argent gagné.</p>
+			</div>
+
+			<div class="workflow--cards">
+				<img src="@/assets/icons/people.svg" alt="Contact" />
+				<p>Vous serez mis directement en relation avec l'équipe. Ni relais ni intermédiaire.</p>
+			</div>
 		</div>
 	</section>
 </template>
 
 <script>
-import CardWorkflow from "@/components/wip/CardWorkflow.vue";
-
 export default {
 	name: "TheWorkFlow",
-	components: {
-		CardWorkflow,
-	},
 };
 </script>
 
@@ -62,6 +56,28 @@ export default {
 
 	@media screen and (min-width: 2550px) {
 		max-width: 55%; // Fixes a bug on big screen
+	}
+}
+
+.workflow--cards {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: var(--green-400);
+	color: var(--text-color-2nd);
+	border-radius: var(--radius-big);
+	padding: 1em;
+	gap: 1em;
+
+	& > img {
+		width: 5em;
+		height: auto;
+		user-select: none;
+	}
+
+	& > p {
+		margin: 0;
+		width: 25ch;
 	}
 }
 </style>
