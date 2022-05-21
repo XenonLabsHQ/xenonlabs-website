ebdc331d4b38de09311d14c214a1945b6a7bb4b8
powerm1nt <nukaworks@nuka.works>
2022-05-21 13:24:06 +0200

    :art: Code Reformat

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
