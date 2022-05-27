f3650408ed44ffbb74b7dcd6d174245f107ce35a
powerm1nt <nukaworks@nuka.works>
2022-05-27 18:56:54 +0200

    :sparkles: Improved member description

--- src/components/wip/CardInfoMember.vue
+++ src/components/wip/CardInfoMember.vue
@@ -3,7 +3,10 @@
 		<img class="info--member__avatar" :src="'src/assets/members/' + img" :alt="name + ' profile picture'" />
 		<div class="info--member--info">
 			<p class="info--member--info__name">{{ name }}</p>
-			<p class="info--member--info__desc">{{ desc }}</p>
+			<p v-if="desc" class="info--member--info__desc">
+				<img alt="badge" src="@/assets/icons/badge.svg" />
+				{{ desc }}
+			</p>
 
 			<base-button class="btn__base__primary">En savoir plus</base-button>
 		</div>
@@ -26,6 +29,7 @@ export default {
 		desc: {
 			type: String,
 			required: false,
+			default: "",
 		},
 		img: {
 			type: String,
@@ -34,6 +38,7 @@ export default {
 		follow: {
 			type: String,
 			required: false,
+			default: "",
 		},
 	},
 };
@@ -62,12 +67,23 @@ export default {
 		align-items: center;
 		gap: 1em;
 		font-weight: 200;
+		color: #fff;
 
 		&__name {
 			margin-block: 0;
 			font-size: 1.2em;
 			font-weight: 500;
-			color: #fff;
+		}
+
+		&__desc {
+			display: flex;
+			align-items: center;
+			gap: 5px;
+
+			img {
+				width: clamp(12px, 1.3vw, 18px);
+				height: clamp(12px, 1.3vw, 18px);
+			}
 		}
 	}
 }
