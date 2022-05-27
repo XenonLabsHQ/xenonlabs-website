1702051db7553f6081a0c78028bfd6f5f428a0c1
powerm1nt <nukaworks@nuka.works>
2022-05-27 21:27:35 +0200

    :bug: Fixed all issues about image imports (Removed component)

--- src/components/wip/TheStaff.vue
+++ src/components/wip/TheStaff.vue
@@ -4,14 +4,60 @@
 			<h2 class="staff__title">Notre équipe</h2>
 
 			<div class="founders">
-				<card-info-member name="Voltis" desc="Fondateur" img="Voltis.gif" />
-				<card-info-member name="Yalis" desc="Co-Fondateur" img="Yalis.webp" />
+				<div class="info--member">
+					<img class="info--member__avatar" src="@/assets/members/Voltis.gif" alt="Voltis Profile Picture" />
+					<div class="info--member--info">
+						<p class="info--member--info__name">Voltis</p>
+						<p class="info--member--info__desc">
+							<img alt="badge" src="@/assets/icons/badge.svg" />
+							Fondateur.
+						</p>
+
+						<base-button class="btn__base__primary">En savoir plus</base-button>
+					</div>
+				</div>
+
+				<div class="info--member">
+					<img class="info--member__avatar" src="@/assets/members/Yalis.webp" alt="Yalis Profile Picture" />
+					<div class="info--member--info">
+						<p class="info--member--info__name">Yalis</p>
+						<p class="info--member--info__desc">
+							<img alt="badge" src="@/assets/icons/badge.svg" />
+							Co-Fondateur.
+						</p>
+
+						<base-button class="btn__base__primary">En savoir plus</base-button>
+					</div>
+				</div>
 			</div>
 
 			<div class="members">
-				<card-info-member name="Taonix" img="Taonix.webp" />
-				<card-info-member name="Powerm1nt" img="Powerm1nt.png" />
-				<card-info-member name="Sigma" img="Sigma.png" />
+				<div class="info--member">
+					<img class="info--member__avatar" src="@/assets/members/Sigma.png" alt="Sigma Profile Picture" />
+					<div class="info--member--info">
+						<p class="info--member--info__name">Sigma</p>
+
+						<base-button class="btn__base__primary">En savoir plus</base-button>
+					</div>
+				</div>
+
+				<div class="info--member">
+					<img class="info--member__avatar" src="@/assets/members/Powerm1nt.png" alt="Powerm1nt Profile Picture" />
+					<div class="info--member--info">
+						<p class="info--member--info__name">Powerm1nt</p>
+
+						<base-button class="btn__base__primary">En savoir plus</base-button>
+					</div>
+				</div>
+
+				<div class="info--member">
+					<img class="info--member__avatar" src="@/assets/members/Taonix.webp" alt="Taonix Profile Picture" />
+					<div class="info--member--info">
+						<p class="info--member--info__name">Taonix</p>
+
+						<base-button class="btn__base__primary">En savoir plus</base-button>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="staff__involving">
@@ -22,12 +68,11 @@
 </template>
 
 <script>
-import CardInfoMember from "@/components/wip/CardInfoMember.vue";
 import BaseButton from "@/components/wip/BaseButton.vue";
 
 export default {
 	name: "TheStaff",
-	components: { BaseButton, CardInfoMember },
+	components: { BaseButton },
 };
 </script>
 
@@ -77,5 +122,48 @@ export default {
 	justify-content: center;
 	flex-wrap: wrap;
 	gap: 1em;
+}
+
+.info--member {
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	padding: 1.2em;
+	background-color: #121511;
+	border-radius: 8px;
+	width: 100%;
+	max-width: 413px;
+	box-sizing: border-box;
+	gap: 1em;
+
+	&__avatar {
+		height: 2.5em;
+		border-radius: 8px;
+	}
+
+	&--info {
+		display: flex;
+		align-items: center;
+		gap: 1em;
+		font-weight: 200;
+		color: #fff;
+
+		&__name {
+			margin-block: 0;
+			font-size: 1.2em;
+			font-weight: 500;
+		}
+
+		&__desc {
+			display: flex;
+			align-items: center;
+			gap: 5px;
+
+			img {
+				width: clamp(12px, 1.3vw, 18px);
+				height: clamp(12px, 1.3vw, 18px);
+			}
+		}
+	}
 }
 </style>
