cabe73f850b717f2c3e61cabdaf0adff45f452b0
sigmanificient <edhyjox@gmail.com>
2022-05-28 23:36:36 +0200

    :fire: Remove old site

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,4 @@ export default [
         name: "home",
         component: () => import("@/views/HomeView.vue"),
     },
-    {
-        path: "/old",
-        name: "old",
-        component: () => import("@/old/views/HomeView.vue"),
-    },
 ];
