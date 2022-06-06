5cb32fbd97dcc50b0dd42db27cda20d5a03944c2
Sigmanificient <edhyjox@gmail.com>
2022-06-07 00:37:31 +0200

    :see_no_evil: Added Missing tags
    Signed-off-by: Sigmanificient <edhyjox@gmail.com>

--- App.vue
+++ App.vue
@@ -1,4 +1,22 @@
 <template>
+	<Head>
+		<Meta charset="utf-8" />
+		<Meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<Meta name="viewport" content="width=device-width,initial-scale=1.0" />
+		<Link rel="icon" href="/favicon.ico" />
+		<Meta name="theme-color" content="#000000" />
+		<Meta name="description" content="XenonLabs: Donnons vie à votre projet" />
+		<Meta property="twitter:site" content="@XenonLabs_" />
+		<Meta property="twitter:creator" content="@XenonLabs_" />
+		<Meta property="twitter:creator:id" content="@XenonLabs_" />
+		<Meta property="og:url" content="https://xenonlabs.fr" />
+		<Meta name="twitter:title" content="XenonLabs: Donnons vie à votre projet" />
+		<Meta name="twitter:card" content="summary" />
+		<Meta name="twitter:description" content="XenonLabs: Donnons vie à votre projet" />
+
+		<Title>XenonLabs.</Title>
+	</Head>
+
 	<router-view />
 </template>
 <style lang="scss">
