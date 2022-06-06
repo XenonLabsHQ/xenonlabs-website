be23f89773d418a3f48ba8e143a59d86ac53d37f
Sigmanificient <edhyjox@gmail.com>
2022-06-07 00:42:54 +0200

    :lipstick: Removed old version styling
    Signed-off-by: Sigmanificient <edhyjox@gmail.com>

--- App.vue
+++ App.vue
@@ -23,10 +23,26 @@
 @import url("https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap");
 
 :root {
-	--bg-default: #294523;
-	--bg-evidence: #74e262;
-	--bg-darker: #5bb04c;
-	--bg-trans-darker: #57575750;
+	--green-400: #74e262;
+	--green-500: #59ae4b;
+	--black-400: #121511;
+	--black-500: #0a0c0a;
+	--white-400: #f0f0f0;
+	--bg: var(--black-500);
+	--bg-2nd: var(--black-400);
+	--text-color: var(--white-400);
+	--text-color-2nd: var(--black-400);
+	--text-title: var(--green-400);
+	--accent: var(--green-500);
+	--accent-2nd: var(--green-400);
+	--radius: 4px;
+	--radius-medium: 15px;
+	--radius-big: 25px;
+}
+
+*::selection {
+	background: var(--accent);
+	color: var(--text-color-2nd);
 }
 
 html {
@@ -36,12 +52,13 @@ html {
 body {
 	margin: 0;
 	font-family: "Outfit", sans-serif;
-	background: var(--bg-darker);
+	font-size: clamp(12px, 1.3vw, 18px);
+	background-color: var(--bg);
+	color: var(--text-color);
+	overflow-x: hidden;
 }
 
-.default {
-	&--alink {
-		text-decoration: none;
-	}
+#app {
+	padding: 0;
 }
 </style>
