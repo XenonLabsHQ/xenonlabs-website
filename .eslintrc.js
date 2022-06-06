c0bd6a0fe127341ba9ba4bd0b36aa178981125d6
Sigmanificient <edhyjox@gmail.com>
2022-06-07 00:01:44 +0200

    :wrench: Updated config files
    Signed-off-by: Sigmanificient <edhyjox@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
     root: true,
-    extends: [
-        "plugin:vue/base",
-        "plugin:vue/vue3-essential",
-        "plugin:vue/vue3-strongly-recommended",
-        "plugin:vue/vue3-recommended",
-        "plugin:prettier/recommended",
-        "eslint:recommended",
-    ],
     env: {
-        node: true,
         browser: true,
-        commonjs: true,
-        amd: true,
+        node: true,
     },
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+    },
+    plugins: ["vue"],
+    extends: [
+        "eslint:recommended",
+        "plugin:prettier/recommended",
+        "plugin:vue/recommended",
+        "plugin:nuxt/recommended",
+    ],
 };
