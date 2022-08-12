6f06dc67fa42ad1f3d1d62c8d9fd3d37b0cfd177
powerm1nt <nukaworks@nuka.works>
2022-08-12 19:20:41 +0200

    :wheelchair: HTML webpage lang is set to fr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  'target': "static"
+  target: "static",
+  head: {
+    htmlAttrs: {
+      lang: "fr", // https://fr.nuxtjs.org/api/configuration-head#htmlattrs
+    },
+  }
 });
