8e4105b60c11c8e7fa00fa0136bb55b20cae9bfc
powerm1nt <nukaworks@nuka.works>
2022-09-07 22:03:16 +0200

    :sparkles: Impl WorlFlow section

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../Common/Components/Modules/Header'
 import Landing from '../Common/Components/Modules/Landing'
 import Staff from '../Common/Components/Modules/Staff'
+import WorkFlow from '../Common/Components/Modules/WorkFlow'
 
 export default function Home() {
   return (
       <>
         <Landing />
+        <WorkFlow />
         <Staff />
       </>
   );
