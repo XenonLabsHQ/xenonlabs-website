d2449e36a67e380180d0009dcd5c97d51bfb80a4
powerm1nt <nukaworks@nuka.works>
2022-09-07 21:19:48 +0200

    :sparkles: Added ButtonScroll.jsx

---
+++ src/Common/Components/Misc/ButtonScroll.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ButtonScroll.scss'
+
+export default function ButtonScroll() {
+  return (
+      <a className="btn__scroll" href="#more">
+        <span/>
+        En savoir plus
+      </a>
+  )
+}
