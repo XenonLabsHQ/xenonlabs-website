6f4a00af6172d58b72d89b15c40b780099b35c04
powerm1nt <nukaworks@nuka.works>
2022-09-07 22:24:21 +0200

    :bug: Fixed path, prevent image won't load

--- src/Common/Components/Modules/Sections/Hero.jsx
+++ src/Common/Components/Modules/Sections/Hero.jsx
@@ -5,7 +5,7 @@ import ButtonScroll from '../../Misc/ButtonScroll'
 export default function Hero() {
   return (
       <div className="hero">
-        <img className="hero__logo" alt="Logo" src="/public/logo.svg"/>
+        <img className="hero__logo" alt="Logo" src="/logo.svg"/>
         <div className="hero__intro">
           <h1 className="hero__title"><span>XenonLabs, </span>Bienvenue !</h1>
           <p className="hero__description">
