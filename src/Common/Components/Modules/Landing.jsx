561c3158f818258445195f33f5ff6eea6ef0aa0d
powerm1nt <nukaworks@nuka.works>
2022-09-07 21:17:44 +0200

    :sparkles: Added Landing.jsx

---
+++ src/Common/Components/Modules/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import QualityCards from './QualityCards'
+import Services from './Services'
+import Hero from '../Misc/Hero'
+import './Landing.scss'
+
+export default function Landing() {
+  return (
+      <section id="whynot" className="landing">
+        <div className="landing__focus">
+          <Hero />
+          <Services />
+        </div>
+        <QualityCards />
+      </section>
+  )
+}
