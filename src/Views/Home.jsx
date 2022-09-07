f6656b0faae64de3361848ce649fcf5ad1e09652
powerm1nt <nukaworks@nuka.works>
2022-09-07 21:20:10 +0200

    :sparkles: Impl Landing

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Common/Components/Modules/Header'
+import Landing from '../Common/Components/Modules/Landing'
 
 export default function Home() {
   return (
       <>
         <Header />
+        <Landing />
       </>
   );
 }
