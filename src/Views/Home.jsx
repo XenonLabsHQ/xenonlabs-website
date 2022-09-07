85267f4fe30810ca1df72cfd483939bb2f7b00d6
powerm1nt <nukaworks@nuka.works>
2022-09-07 21:52:55 +0200

    :sparkles: Impl Staff section

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Common/Components/Modules/Header'
 import Landing from '../Common/Components/Modules/Landing'
+import Staff from '../Common/Components/Modules/Staff'
 
 export default function Home() {
   return (
       <>
         <Header />
         <Landing />
+        <Staff />
       </>
   );
 }
